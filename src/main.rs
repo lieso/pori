14d626cc2c0dbdf9130bedb4faac2d520ed9935a
David Roussov <david.roussov@gmail.com>
2025-11-24 14:44:31 +1100

    add a web browser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use ratatui::{text::Text, Frame};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use headless_chrome::{Browser, LaunchOptions};
 
 fn main() {
     let mut terminal = ratatui::init();
 
     let mut input = String::new();
+
+
+
+
+
+    let browser = Browser::new(
+       LaunchOptions {
+           headless: true,
+           ..Default::default()
+       }
+    ).expect("Could not launch browser");
+
+    let tab = browser.new_tab().expect("Could not create new tab");
+
+    tab.navigate_to("https://www.rust-lang.org/").expect("Could not navigate");
+    tab.wait_until_navigated().expect("Could not wait until navigate");
+
+
+
+
+
+
 
     loop {
         terminal.draw(draw).expect("failed to draw frame");
