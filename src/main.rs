a2907b91eb68c79c4d2ca2ff38c0d8744be633e4
David Roussov <david.roussov@gmail.com>
2025-11-24 13:29:34 +1100

    q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use crossterm::event::{self, Event};
 use ratatui::{text::Text, Frame};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 
 fn main() {
     let mut terminal = ratatui::init();
+
+    let mut input = String::new();
+
     loop {
         terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break;
+
+        match event::read().expect("Could not read event") {
+            Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+                KeyCode::Char('q') => break,
+                _ => {}
+            },
+            _ => {}
         }
+
     }
     ratatui::restore();
 }
